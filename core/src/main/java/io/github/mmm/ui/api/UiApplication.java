9aaea6f4361894b69a9e5178955c926e735cd42d
Jörg Hohwiller <hohwille@users.sourceforge.net>
2020-04-26 15:58:36 +0200

    moved here from spi

---
+++ core/src/main/java/io/github/mmm/ui/api/UiApplication.java
@@ -0,0 +1,17 @@
+/* Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0 */
+package io.github.mmm.ui.api;
+
+/**
+ * Interface for a UI application (client with user-interface).
+ *
+ * @since 1.0.0
+ */
+public interface UiApplication {
+
+  /**
+   * Starts this application.
+   */
+  void start();
+
+}
