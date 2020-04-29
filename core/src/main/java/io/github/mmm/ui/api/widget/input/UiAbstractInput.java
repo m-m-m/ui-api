6743f784623352f69ef027343bffededf12de842
Jörg Hohwiller <hohwille@users.sourceforge.net>
2020-04-29 14:46:00 +0200

    fixed JavaDoc

--- core/src/main/java/io/github/mmm/ui/api/widget/input/UiAbstractInput.java
+++ core/src/main/java/io/github/mmm/ui/api/widget/input/UiAbstractInput.java
@@ -16,8 +16,8 @@ import io.github.mmm.ui.api.widget.value.UiValuedWidget;
 public abstract interface UiAbstractInput<V> extends UiValidatableWidget<V> {
 
   /**
-   * @return the {@link UiLabel#getText() label} for the {@link UiInput#getNameWidget() name widget} or the title of
-   *         the {@link io.github.mmm.ui.api.widget.form.UiFormGroup}.
+   * @return the {@link UiLabel#getText() label text} for the {@link UiInput#getNameWidget() name widget} or the title
+   *         of the form group.
    */
   String getName();
 
