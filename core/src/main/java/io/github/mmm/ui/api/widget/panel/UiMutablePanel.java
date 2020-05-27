492b2dd0cff4a5cc4f1801eb2bf572f20249879e
Jörg Hohwiller <hohwille@users.sourceforge.net>
2020-05-27 22:23:38 +0200

    m-m-m/ui-api#4: add generic support to add inputs

--- core/src/main/java/io/github/mmm/ui/api/widget/panel/UiMutablePanel.java
+++ core/src/main/java/io/github/mmm/ui/api/widget/panel/UiMutablePanel.java
@@ -4,6 +4,7 @@ package io.github.mmm.ui.api.widget.panel;
 
 import io.github.mmm.ui.api.widget.UiRegularWidget;
 import io.github.mmm.ui.api.widget.composite.UiMutableComposite;
+import io.github.mmm.ui.api.widget.input.UiInput;
 
 /**
  * {@link UiMutableComposite} for a generic panel that accepts any {@link UiRegularWidget} as {@link #getChild(int)
@@ -12,5 +13,14 @@ import io.github.mmm.ui.api.widget.composite.UiMutableComposite;
  * @since 1.0.0
  */
 public abstract interface UiMutablePanel extends UiMutableComposite<UiRegularWidget>, UiRegularWidget {
+
+  /**
+   * @param input the {@link UiInput} to add as {@link UiInput#getContainerWidget() container widget} (including its
+   *        label).
+   */
+  default void addChild(UiInput<?> input) {
+
+    addChild(input.getContainerWidget());
+  }
 
 }
