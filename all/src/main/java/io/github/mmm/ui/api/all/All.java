6e6d9e98dabdae0a7fadfc49867ff1960c5ddb19
Jörg Hohwiller <hohwille@users.sourceforge.net>
2021-01-22 17:59:17 +0100

    added dummy class to prevent JavaDoc error breaking deployment process

---
+++ all/src/main/java/io/github/mmm/ui/api/all/All.java
@@ -0,0 +1,14 @@
+/* Copyright (c) The m-m-m Team, Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0 */
+package io.github.mmm.ui.api.all;
+
+/**
+ * JavaDoc fails to generate when there is no public class or interface. Maven deployment to central fails when there is
+ * no JavaDoc provided with the release. To allow maven and JavaDoc to build some pointless JavaDoc for this aggregation
+ * module, this class was added.
+ *
+ * @since 1.0.0
+ */
+public class All {
+
+}
